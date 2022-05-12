0aaec76cc69bb39cb4aa5c62052773904e1f8682
gtineo07 <g.tineo11@gmail.com>
2022-05-12 13:53:05 -0400

    se corrio error en crar usurio

--- src/scripts/set-default-user.ts
+++ src/scripts/set-default-user.ts
@@ -1,6 +1,6 @@
 import { getRepository } from 'typeorm';
 import { ConfigService } from '@nestjs/config';
-import { DEFAULT_USER_EMAIL, DEFAULT_USER_PASSWORD } from '../config/constants';
+import { DEFAULT_USER_EMAIL, DEFAULT_USER_PASSWORD, DEFAULT_USER_NAME, DEFAULT_USER_LAST_NAME } from '../config/constants';
 import { User } from 'src/user/entities';
 
 const setDefaultUser = async (config: ConfigService) => {
@@ -15,6 +15,8 @@ const setDefaultUser = async (config: ConfigService) => {
 
   if (!defaultUser) {
     const adminUser = userRepository.create({
+      name: config.get<string>(DEFAULT_USER_NAME),
+      lastName: config.get<string>(DEFAULT_USER_LAST_NAME),
       email: config.get<string>(DEFAULT_USER_EMAIL),
       password: config.get<string>(DEFAULT_USER_PASSWORD),
       roles: ['ADMIN'],
